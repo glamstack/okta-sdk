c99df08efbbc642fbfe0edf4410fb3a5c4155405
Jeff Martin <jmartin@gitlab.com>
2022-01-18 17:08:17 -0800

    Add `src/Config/glamstack-okta` scaffolding with `log_channels`

---
+++ src/Config/glamstack-okta.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /**
+     * Log Channel Name
+     * ------------------------------------------------------------------------
+     * Throughout the SDK, we use the config('glamstack-okta.log_channels')
+     * array variable to allow you to set the log channels (custom log stack)
+     * that you want API logs to be sent to.
+     *
+     * If you leave this at the value of `['single']`, all API call logs will
+     * be sent to the default log file for Laravel that you have configured
+     * in config/logging.php which is usually storage/logs/laravel.log.
+     *
+     * If you would like to see Okta API logs in a separate log file that
+     * is easier to triage without unrelated log messages, you can create a
+     * custom log channel and add the channel name to the array. For example,
+     * we recommend creating a custom channel with the name `glamstack-okta`,
+     * however you can choose any name you would like.
+     * Ex. ['single', 'glamstack-okta']
+     *
+     * You can also add additional channels that logs should be sent to.
+     * Ex. ['single', 'glamstack-okta', 'slack']
+     *
+     * https://laravel.com/docs/8.x/logging
+     */
+
+    'log_channels' => ['single'],
+
+];
