87452f320d14102d86dcfe95c9ab83c32e984a1e
Dillon Wheeler <dwheeler@gitlab.com>
2022-02-01 14:08:43 +0000

    Update `ApiClient` to make `$api_token` and `$base_url` class property not nullable

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -14,8 +14,8 @@ class ApiClient
 
     const API_VERSION = 1;
 
-    private ?string $api_token;
-    private ?string $base_url;
+    private string $api_token;
+    private string $base_url;
     private array $connection_config;
     private string $connection_key;
     private array $request_headers;
